ab51cf5759d803acaf9e91786e42f6962590176f
Steve Hunter <steve.hunter@bigdogconsultants.co.uk>
2014-02-17 17:33:46 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BigDog::Application.config.secret_key_base = 'a69dfe0ed44dd3668efd6cd4ca9f68ad914fee61b9064465b0eb42d0d71b768d337e530fef1548701813e41f7f91f01f84e1149c2587de1bee83add0640905de'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BigDog::Application.config.session_store :cookie_store, key: '_big_dog_session'
